d1020e7c52c0677d583e0c6188a48461d04320b8
johnston <ethan.john.13579@gmail.com>
2026-06-12 00:54:57 +0400

    if bad response recieved, response text is logged

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import requests
 import os
 
 while 1:
-    actual = requests.get("https://ipinfo.io/").json()["ip"]
-    known = requests.get("https://raw.githubusercontent.com/johnstonalt/ip-updates/refs/heads/main/README.md").text.replace("\n", "").replace("# ", "")
+    r1 = requests.get("https://ipinfo.io/")
+    r2 = requests.get("https://raw.githubusercontent.com/johnstonalt/ip-updates/refs/heads/main/README.md")
+
+    try:
+        actual = r1.json()["ip"]
+        known = r2.text.replace("\n", "").replace("# ", "")
+    except:
+        print(f"recieved bad response\nrequest to get actual IP returned:\n{r1.text}\n\nresponse to get known IP returned:\n{r2.text}")
 
     if actual != known:
         print(f"\nNew public IP address: {known} -> {actual}\n")
