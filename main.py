21adf6f8f36bd746bc7fd7361764ee3368ce5ab9
johnston <ethan.john.13579@gmail.com>
2026-06-19 14:30:49 +0400

    new IP

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import requests
 import os
 
+"""
 while 1:
     # python is propaganda
     actual = ""
@@ -34,3 +35,32 @@ while 1:
     time.sleep(60*5)
 
 # if you're seeing this, text John the number 69420! 
+"""
+
+known = ""
+kr = requests.get("https://raw.githubusercontent.com/johnstonalt/ip-updates/refs/heads/main/README.md")
+known = kr.text.replace("\n", "").replace("# ", "")
+
+while 1:
+    actual = ""
+    failed = False
+
+    try:
+        r = requests.get("http://api.ipify.org")
+        actual = r.text
+    except:
+        print("failed to get actual IP address")
+        failed = True
+
+    if failed == False:
+        if actual != known:
+            print(f"\nNew public IP address: {known} -> {actual}\n")
+
+            with open("README.md", "w") as f:
+                f.write(f"# {actual}")
+            f.close()
+
+            os.system("git add .; git commit -m \"new IP\"; git push origin main")
+            known = actual
+ 
+    time.sleep(60)
